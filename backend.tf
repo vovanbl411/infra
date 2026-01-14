8cd4ad720d94862644b6bcaa657fed957ca79ffb
Vladimir Korovayevich <vovanbl411@yandex.by>
2026-01-14 22:39:37 +0300

    change workflow file stage terraform init

--- backend.tf
+++ backend.tf
@@ -1,12 +1,12 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state"
-    key    = "kubernetes-cluster/terraform.tfstate"
-    
-    # Эти параметры необходимы для R2
+    bucket                      = "terraform-state"
+    key                         = "kubernetes-cluster/terraform.tfstate"
     skip_credentials_validation = true
     skip_region_validation      = true
     skip_metadata_api_check     = true
-    force_path_style            = true
- }
+    use_path_style              = true
+    skip_requesting_account_id  = true
+    # endpoint и region будут заданы в workflow
+  }
 }
