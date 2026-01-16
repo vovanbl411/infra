1bdbb6cc795425a50a86802e447715da25868e4f
Vladimir Korovayevich <vovanbl411@yandex.by>
2026-01-16 22:25:17 +0300

    delete .terraform and change .gitignore

--- backend.tf
+++ backend.tf
@@ -7,7 +7,7 @@ terraform {
     skip_metadata_api_check     = true
     use_path_style              = true
     skip_requesting_account_id  = true
-    region                      = "us-east-1"
+    region                      = "auto"
     # endpoint будут заданы в workflow
   }
 }
